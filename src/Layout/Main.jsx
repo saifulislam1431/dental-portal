35a3e26a31f6c09687669c1677f99a62ba5cb282
Saiful Islam <sakibbhaiya143@gmail.com>
2023-06-05 22:56:50 +0600

    Page set up

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+import Footer from '../Pages/Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <section>
+            <Navbar></Navbar>
+            <Footer></Footer>
+        </section>
+    );
+};
+
+export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#07332F",
+        
+"secondary": "#F7A582",
+        
+"accent": "#F1F5F9",
+        
+"neutral": "#2b1d2f",
+        
+"base-100": "#ffffff",
+        
+"info": "#8498e1",
+        
+"success": "#7fe6d0",
+        
+"warning": "#f3c672",
+        
+"error": "#f22e1c",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
